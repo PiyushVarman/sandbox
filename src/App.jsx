20be9b305493981741776ce7720a412ca7581e84
Piyush Varman <varmanpiyush@gmail.com>
2026-04-28 16:45:24 +0530

    feat: Tooltips

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import {
   MenubarTrigger,
 } from "@/components/ui/menubar"
 
-import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import { Tooltip, TooltipContent, TooltipTrigger, TooltipProvider } from "@/components/ui/tooltip";
 import { Menu } from 'lucide-react';
 
 function App() {
@@ -38,11 +38,19 @@ function App() {
   }, []);
 
   return (
+    <TooltipProvider>
     <div>
     {grid && <RetroGrid className='absolute top-0 bottom-0 z-[-1]' darkLineColor="#ffd700" lightLineColor="#ffd700" opacity={0.5} angle={angle}/>}
     <Menubar className="absolute w-full flex flex-row items-center justify-start z- rounded-none backdrop-blur text-white border-none">
     <MenubarMenu>
-      <MenubarTrigger>🥔</MenubarTrigger>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <MenubarTrigger>🥔</MenubarTrigger>
+        </TooltipTrigger>
+        <TooltipContent className='text-[#ffd700]!' side='bottom'>
+          AaloOS
+        </TooltipContent>
+      </Tooltip>
       <MenubarContent className="mx-1 backdrop-blur-xl bg-black/50 text-white">
         <MenubarGroup>
           <MenubarItem onClick={()=>(dockShow==true)?setDockShow(false):setDockShow(true)}>{(dockShow==true)?"Hide ":"Show "}Dock</MenubarItem>
@@ -69,7 +77,14 @@ function App() {
       </MenubarContent>
     </MenubarMenu>
     <MenubarMenu>
-      <MenubarTrigger className="float-right">{now.toLocaleTimeString()}</MenubarTrigger>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <MenubarTrigger className="float-right">{now.toLocaleTimeString()}</MenubarTrigger>
+        </TooltipTrigger>
+        <TooltipContent className='tracking-tight!'>
+          Date and Time
+        </TooltipContent>
+      </Tooltip>
       <MenubarContent className="p-2 backdrop-blur-xl w-full text-center flex flex-col justify-center bg-black/50 text-white ">
         {now.toLocaleDateString('en-IN',options)}
         <Calendar className="rounded-xl" mode="single" selected={date} onSelect={setDate} />
@@ -95,11 +110,27 @@ function App() {
       
     <AnimatePresence>
     {dockShow && <motion.div initial={{opacity:0.5, scale:0.9}} animate={{opacity: 1, scale:1}} exit={{scale:0.25, opacity:0}} transition={{duration: 0.1}} className='flex flex-row items-center justify-center gap-x-5  absolute bottom-4 left-[35%] w-[30vw] h-[75px] rounded-2xl shadow-xs *:hover:shadow-md *:hover:shadow-black shadow-black bg-black/50 hover:bg-black/60 duration-200 backdrop-blur-sm select-none *:flex *:items-center *:justify-center *:hover:scale-110 *:rounded-xl *:w-13 *:h-13 *:active:shadow-none *:active:scale-100'>
-        <motion.div initial={{opacity:0.5, scale:0.8}} animate={{opacity: 1, scale:1}} className='outline text-s align-middle bg-white duration-200 p-0 cursor-pointer' onClick={() => (calcOpen == false ? setCalcOpen(true) : setCalcOpen(false))}>➕➖<br/>✖️➗</motion.div>
-        <motion.div initial={{opacity:0.5, scale:0.8}} animate={{opacity: 1, scale:1}} className='text-3xl align-middle bg-blue-300 duration-200 p-0 cursor-pointer' onClick={() => (notepadOpen == false ? setNotepadOpen(true) : setNotepadOpen(false))}>🗒️</motion.div>
+      
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <motion.div initial={{opacity:0.5, scale:0.8}} animate={{opacity: 1, scale:1}} className='outline text-s align-middle bg-white duration-200 p-0 cursor-pointer' onClick={() => (calcOpen == false ? setCalcOpen(true) : setCalcOpen(false))}>➕➖<br/>✖️➗</motion.div>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Calculator</p>
+          </TooltipContent>
+        </Tooltip>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <motion.div initial={{opacity:0.5, scale:0.8}} animate={{opacity: 1, scale:1}} className='text-3xl align-middle bg-blue-300 duration-200 p-0 cursor-pointer' onClick={() => (notepadOpen == false ? setNotepadOpen(true) : setNotepadOpen(false))}>🗒️</motion.div>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Notepad</p>
+          </TooltipContent>
+        </Tooltip>
     </motion.div> }
     </AnimatePresence>
     </div>
+    </TooltipProvider>
   )
 }
 
